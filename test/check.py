5b682780cd0129264a23f6cdb9f1258bd87ddf05
jovishaw <jiweixiao13@gmail.com>
2024-03-06 17:34:30 +0800

    FIX: load and run efiXplorer with argument "2"

--- test/check.py
+++ test/check.py
@@ -6,15 +6,22 @@ import shutil
 
 
 def clean_result():
-    for i in glob.glob(os.path.join(os.path.expanduser("~/efidrill_workspace"),"*/res.json"),recursive=True):
+    for i in glob.glob(
+        os.path.join("D:\\efidrill_workspace\\", "*\\res.json"), recursive=True
+    ):
         with open(i) as f:
             s = f.read()
             f.close()
         if s == "{}":
-            dir,name = os.path.split(i)
-            shutil.move(dir,dir+"_empty")
+            dir, name = os.path.split(i)
+            shutil.move(dir, dir + "_empty")
 
-    os.system(f"zip -r9 {os.path.join(os.path.expanduser('~/efidrill_workspace'),'empty.zip')} {os.path.join(os.path.expanduser('~/efidrill_workspace'),'*_empty')}")
-    directories = glob.glob(os.path.join(os.path.expanduser("~/efidrill_workspace"),"*_empty"))
+    # os.system(f"zip -r9 {os.path.join(os.path.expanduser('~/efidrill_workspace'),'empty.zip')} {os.path.join(os.path.expanduser('~/efidrill_workspace'),'*_empty')}")
+    directories = glob.glob(os.path.join("D:\\efidrill_workspace\\", "*_empty"))
     for directory in directories:
         shutil.rmtree(directory)
+
+
+if __name__ == "__main__":
+    clean_result()
+    print("cleaned")
--- test/run.py
+++ test/run.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env/ python
 import os
 import subprocess
-ida_path = "/Applications/IDA Pro 8.2/idabin/idat64"
-work_dir = os.path.abspath('./modules')
+
+ida_path = "idat64"
+work_dir = os.path.abspath("./modules")
 script_path = os.path.abspath("./analysis.py")
 
-moduels = []
-moduels.append(os.listdir(os.path.join(work_dir,"smm")))
-moduels.append(os.listdir(os.path.join(work_dir,"dxe")))
-for file in moduels:
-    # cmd_str = ida.exe -Lida.log -c -A -Sanalysis.py pefile
-    cmd_str = f'"{ida_path}" -Lida.log -c -A -S{script_path} {os.path.join(work_dir, file)}'
-    print(cmd_str)
-    p = subprocess.Popen((cmd_str),shell=True)
-    p.wait()
+modules = []
+modules.extend(
+    [
+        os.path.join(work_dir, "smm", file)
+        for file in os.listdir(os.path.join(work_dir, "smm"))
+    ]
+)
+modules.extend(
+    [
+        os.path.join(work_dir, "dxe", file)
+        for file in os.listdir(os.path.join(work_dir, "dxe"))
+    ]
+)
 
-import check
-check.clean_result()
+for file in modules:
+    # cmd_str = ida.exe -Lida.log -c -A -Sanalysis.py pefile
+    cmd_str = f"{ida_path} -Lida.log -c -A -S{script_path} {file}"
+    print(cmd_str)
+    p = subprocess.Popen(cmd_str, shell=True)
+    p.wait()
