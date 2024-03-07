2d086d867abea100d35dbc9681ca5e4b2ee2224e
jovishaw <jiweixiao13@gmail.com>
2024-03-07 11:26:13 +0800

    format: format code in in folder efidrill/arch

--- efidrill/arch/x64.py
+++ efidrill/arch/x64.py
@@ -1,14 +1,14 @@
 class x64:
     arch_name = "x64"
-    function_param = ["RCX","RDX","R8","R9"]
-    register_list = ["RAX", #0
-                     "RCX", #1
-                     "RDX", #2
-                     "RBX", #3
-                     "RSP", #4
-                     "RBP", #5
-                     "RSI", #6
-                     "RDI", #7
+    function_param = ["RCX", "RDX", "R8", "R9"]
+    register_list = ["RAX",  # 0
+                     "RCX",  # 1
+                     "RDX",  # 2
+                     "RBX",  # 3
+                     "RSP",  # 4
+                     "RBP",  # 5
+                     "RSI",  # 6
+                     "RDI",  # 7
                      "R8",
                      "R9",
                      "R10",
@@ -24,12 +24,12 @@ class x64:
     sub_name = 'sub'
     cmp_name = 'cmp'
     and_name = 'and'
-    stack_register = ['RSP',"RBP","R11"]
-    bigger_jmp = ['ja','jnbe']
-    small_jmp = ['jb',]
+    stack_register = ['RSP', "RBP", "R11"]
+    bigger_jmp = ['ja', 'jnbe']
+    small_jmp = ['jb', ]
 
-    equal_jmp = ['jz','je']
-    unequal_jmp = ['jnz','jne']
-    not_bigger_jmp = ['jbe','jna']
+    equal_jmp = ['jz', 'je']
+    unequal_jmp = ['jnz', 'jne']
+    not_bigger_jmp = ['jbe', 'jna']
     not_small_jmp = ['jnb']
     lib_point_size = 0x8
