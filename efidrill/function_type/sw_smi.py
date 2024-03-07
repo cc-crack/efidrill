b82c67eb5b927524c879e82cb78b71ede63c5ba2
jovishaw <jiweixiao13@gmail.com>
2024-03-07 13:00:49 +0800

    format: format code in in folder efidrill/function_type

--- efidrill/function_type/sw_smi.py
+++ efidrill/function_type/sw_smi.py
@@ -1,14 +1,10 @@
-
-import angr
 from efidrill.function_type.function_type import Function_type
-from efidrill.config import config
-class Sw_Smi(Function_type):
 
+
+class Sw_Smi(Function_type):
 
     @staticmethod
     def check_function(ea, name):
         if name.find("ChildSwSmiHandler") == -1 and name.find("SwSmiHandler") != -1:
             return 1
         return 0
-
-    
